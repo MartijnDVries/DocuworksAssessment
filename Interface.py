ba15560ad610da636fbaba13d610e663d18a3c3d
Jeboibrummels <389martijn@gmail.com>
2022-10-13 20:43:21 +0200

    Added search function to DocumentReader class

--- Interface.py
+++ Interface.py
@@ -1,21 +1,38 @@
+import sys
 import click
 from ReadWriteDocument import DocumentReader
 
 
 Docu = DocumentReader()
 
-@click.command()
-@click.option('--filename', default='text', prompt="Enter the name of the file")
-def file_name(filename):
+
+@click.group(help="yo")
+@click.option('--filename', prompt="In which file do you want to search? ", help="Enter filename without filetype")
+@click.option('--search', prompt="Which word do you wint to find? ", help="Enter the word you want to search in the text (Case-sensitive)")
+def app():
+    app()
+
+@app.command()
+def print_file(filename):
     click.echo(Docu.readfile(filename))
 
+@app.command()
+def search_in_file(filename, search):
+    click.echo(Docu.search(filename, search))
 
-@click.command()
-@click.option('--option', default="read", prompt="What do you want to do?")
+
+@cli.command()
+@click.option('--option', prompt="What do you want to do?")
 def app(option):
-    if option == "read":
-        file_name()
+    if option == "quit":
+        sys.exit()
+    elif option == "read":
+        print_file()
+    elif option == "search":
+        search_in_file()
+
+
 
 
 if __name__ == "__main__":
-    app()
+    cli()
--- ReadWriteDocument.py
+++ ReadWriteDocument.py
@@ -1,24 +1,29 @@
-import io
-
-import click
-import json
-
+import re
 
 class DocumentReader:
 
     def __init__(self):
         self.path = "C:\\Python\\DocuworksAssessment\\TextFiles\\"
 
-    def readfile(self, file):
+    def readfile(self, file : str) -> str:
         with open(f"{self.path}{file}.txt", 'r') as f:
             txt = f.read()
+        return txt.strip()
 
-        print(txt.strip())
-
-    def savefile(self, file):
+    def savefile(self, file : str):
         with open(f"{self.path}{file}.txt", 'w') as f:
             f.write(file)
+
+    def search(self, file: str, search : str) -> list:
+        with open(f"{self.path}{file}.txt", 'r') as f:
+            txt = f.read()
+        word_index_list = [word.span() for word in re.finditer(search, txt.strip())]
+        return word_index_list
+
+
 
 
 if __name__ == "__main__":
     d = DocumentReader()
+    # print(d.readfile('text'))
+    print(d.search('text', 'Otje'))
