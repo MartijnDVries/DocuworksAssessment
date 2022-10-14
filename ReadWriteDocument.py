cb406afbd84d72e83ef8ff2a6fb891dad9982a9f
Jeboibrummels <389martijn@gmail.com>
2022-10-14 22:49:54 +0200

    Added find email address function to DocumentReader class

--- ReadWriteDocument.py
+++ ReadWriteDocument.py
@@ -25,14 +25,14 @@ class DocumentReader:
         return [word.span() for word in re.finditer(search, txt.strip())]
 
     def replace(self, file: str, search: str, replace: str) -> str:
-        """replace all occurrences of a word with a new word"""
+        """replace all occurrences of a word in a textfile with another word"""
         print(search)
         with open(f"{self.path}{file}.txt", 'r') as f:
             txt = f.read()
         return re.sub(fr"\b{search}\b", replace, txt.strip())
 
     def common_word(self, file: str) -> list:
-        """Find the most common word in the text"""
+        """Find the most common word in a textfile"""
         with open(f"{self.path}{file}.txt", 'r') as f:
             txt = f.read()
         word_list = txt.split()
@@ -59,7 +59,7 @@ class DocumentReader:
             count_list = [word, count]
         return self.find_common_word(word_list, count_list)
 
-    def palindromes(self, file: str):
+    def palindromes(self, file: str) -> list:
         """Find all palindromes in a text file and return in a list"""
         with open(f"{self.path}{file}.txt", 'r') as f:
             txt = f.read()
@@ -68,7 +68,6 @@ class DocumentReader:
         if not palindrome_list:
             raise NoPalindromeError
         return palindrome_list
-
 
     def find_palindromes(self, word_list: list, palindromes_list: list = None) -> list:
         """Continues palindromes function"""
@@ -92,6 +91,14 @@ class DocumentReader:
         word_list = [not_word for not_word in word_list if not_word != potential_palindrome]
         return self.find_palindromes(word_list, palindromes_list)
 
+    def find_email_address(self, file: str) -> list:
+        """list all the email addresses in a file"""
+        with open(f"{self.path}{file}.txt", 'r') as f:
+            txt = f.read()
+        return re.findall(r'([A-Za-z0-9_-]+\.?[A-Za-z0-9_-]+@[A-Za-z0-9]+\.?[A-Za-z]+\.?[A-Za-z]+)', txt)
+
+
+
 
 
 if __name__ == "__main__":
@@ -99,6 +106,7 @@ if __name__ == "__main__":
     # print(d.readfile('text'))
     # print(d.replace('text', 'een', ''))
     # print(d.common_word('text'))
-    print(d.palindromes('text'))
+    #print(d.palindromes('text'))
+    print(d.find_email_address('text'))
     # print(d.replace('text', 'publiciteit', ' '))
-    # print(os.getcwd())
+
